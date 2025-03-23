105c5d2cae4d3ab7cf19da80b11f26ab9ea41db7
Bertrand935 <bertrand.labrecque@gmail.com>
2025-03-23 17:22:25 -0400

    clean na

--- nettoyage_data.R
+++ nettoyage_data.R
@@ -3,8 +3,8 @@
 clean_na <- function(df) {
   df[df == ''] <- NA# Remplace les cases vides ou "Na" par NA
 
-  df<- df %>% mutate(obs_variable= recode(obs_variable, " pr@#sence " = "presence")) #correction d'erreur d'orthographe
-  df <- df%>%mutate(time_obs=recode(time_obs, "0" = "Na")) #remplacement seule donnée "0" par NA
-
+  df$obs_variable <- gsub("pr@#sence", "presence", df$obs_variable) #correction d'erreur d'orthographe
+  df$time_obs <- gsub("0", "NA", df$time_obs) #remplacement seule donnée "0" par NA
+  #df <- df%>%mutate(time_obs=recode(time_obs, "0" = "Na")) 
   return(df)
   }
