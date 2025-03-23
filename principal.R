f752da076f281d61f4e127c3a936a843f4e39402
Bertrand935 <bertrand.labrecque@gmail.com>
2025-03-23 14:49:39 -0400

    principal

---
+++ principal.R
@@ -0,0 +1,17 @@
+setwd("")
+source("appel_data.R")
+source("table_primaire.R")
+source("table_date.R")
+source("table_site.R")
+source("table_espece.R")
+source("event_table.R")
+
+Data <- grosse_tab("/lepidopteres")
+#définir le chemin pour le dossier lepidopteres
+Data<-concordance(Data)
+Data <- clean_na(Data)
+
+tab_prim <- tab_primaire(Data)
+tab_site <- create_site_table(tab_prim, Data)
+tab_esp <- table_esp(Data)
+tab_event <- create_event_table(tab_prim, Data)
