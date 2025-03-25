9ced1c50e7f9dc6c61bcc9992f0b83e4a5b639e7
MarikaRoberge <robm1206@usherbrooke.ca>
2025-03-25 17:15:49 -0400

    changement colonne_type

--- colonne_type.R
+++ colonne_type.R
@@ -1,37 +1,7 @@
-library(readr)
-library(dplyr)
-
-# Définition des types de colonnes
-col_types <- cols(
-  observed_scientific_name = col_character(),
-  year_obs = col_integer(),
-  day_obs = col_integer(),
-  time_obs = col_character(),      # On laisse en character (pour le moment)
-  dwc_event_date = col_character(),  # On laisse en character (pour le moment)
-  obs_variable = col_character(),
-  obs_unit = col_character(),
-  obs_value = col_double(),
-  lat = col_double(),
-  lon = col_double(),
-  original_source = col_character(),
-  creator = col_character(),
-  title = col_character(),
-  publisher = col_character(),
-  intellectual_rights = col_character(),
-  license = col_character(),
-  owner = col_character()
-)
-
-# Fonction pour charger un fichier et appliquer les corrections
-type_colonne <- function(file_path) {
-  df <- read.csv(file_path, col_types = col_types, col_names = TRUE, na = c("", "NA"))  #ne marche pas 
-  return(df)  # Pas de conversion des dates ici
-}
-
 library(dplyr)
 #Fonction qui définit les types de colonnes :
 
-col_types <- function(Brute) {
+type_colonne <- function(Brute) {
   Brute %>%
     mutate(
       observed_scientific_name = as.character(observed_scientific_name),
