52adcae747a5531f395b4a9a2e6945d6badf1052
Bertrand935 <bertrand.labrecque@gmail.com>
2025-03-31 17:24:16 -0400

    qq modifs

--- table_site.R
+++ table_site.R
@@ -4,6 +4,6 @@
     # Utiliser les colonnes lat et lon de Brute (appel)
     tab_site <- cbind(primaire$unique_id, appel$lat, appel$lon)
     tab_site <- as.data.frame(tab_site)
-    colnames(tab_site) <- c("unique_id", "lat", "lon") #renommer les colonnes (mettre site_id si jamais au lieu de unique_id)
+    colnames(tab_site) <- c("id_site", "lat", "lon") #renommer les colonnes (mettre site_id si jamais au lieu de unique_id)
     return(tab_site)
   }
