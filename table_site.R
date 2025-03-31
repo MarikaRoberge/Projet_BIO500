117e052cb9207ba3142240d6354eba68061614b6
MarikaRoberge <robm1206@usherbrooke.ca>
2025-03-31 14:30:42 -0400

    modifs

--- table_site.R
+++ table_site.R
@@ -2,8 +2,8 @@
 
   create_site_table <- function(primaire, appel) { 
     # Utiliser les colonnes lat et lon de Brute (appel)
-    site_table <- cbind(primaire$unique_id, appel$lat, appel$lon)
-    site_table <- as.data.frame(site_table)
-    colnames(site_table) <- c("unique_id", "lat", "lon") #renommer les colonnes
-    return(site_table)
+    tab_site <- cbind(primaire$unique_id, appel$lat, appel$lon)
+    tab_site <- as.data.frame(tab_site)
+    colnames(tab_site) <- c("unique_id", "lat", "lon") #renommer les colonnes
+    return(tab_site)
   }
