ddc8ec4e6687a997614025210844e58cd42ee9f9
MarikaRoberge <robm1206@usherbrooke.ca>
2025-03-31 16:04:39 -0400

    modifs

--- Injecter_site.R
+++ Injecter_site.R
@@ -1,5 +1,13 @@
-#Injecter les données dans le tableau secondaire site
-insert_site <- function(con, data) {
-  sites_unique <- unique(data[, c("auteur", "statut", "institution", "ville", "pays")])
-  dbWriteTable(con, "Sites", sites_unique, append = TRUE, row.names = FALSE)
-}
+connect_db <- function(db_name = "biodiversite.sqlite") {
+  dbConnect(SQLite(), dbname = db_name)
+  con<- dbConnect(SQLite(), dbname="lepido.db") #à vérifier
+
+  
+#Injecter les données dans les tables
+inject_data <- function(con, data) {
+  dbWritetable(con, "tab_primaire")
+  dbWriteTable(con, "tab_site", , append = TRUE, row.names = FALSE)
+  dbWriteTable(con, "tab_date, sit")
+  
+    }
+  }
