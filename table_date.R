7ffd9708399858122f32dfcbfdbeab2a7554aaa3
MarikaRoberge <robm1206@usherbrooke.ca>
2025-03-31 21:44:23 -0400

    suppression de 4 scripts (3 pour des fonctions pour créer les tables (primaire et deux secondaires) et un autre script ou on tentait d'injecter les données auparavant).

--- table_date.R
+++
@@ -1,8 +0,0 @@
-# Fonction pour créer une table secondaire donnant des détails sur la date
-create_table_date <- function(primaire, data) { 
-  
-  tab_date <- cbind(primaire$unique_id, data$year_obs, data$day_obs, data$time_obs, data$dwc_event_date)
-  colnames(tab_date) <- c("unique_id", "year_obs", "day_obs", "time_obs")
-  
-  return(as.data.frame(tab_date))
-}
--- table_site.R
+++
@@ -1,9 +0,0 @@
-# Fonction pour créer une table secondaire avec les détails du site
-
-  create_site_table <- function(primaire, appel) { 
-    # Utiliser les colonnes lat et lon de Brute (appel)
-    tab_site <- cbind(primaire$unique_id, appel$lat, appel$lon)
-    tab_site <- as.data.frame(tab_site)
-    colnames(tab_site) <- c("unique_id", "lat", "lon") 
-    return(tab_site)
-  }
